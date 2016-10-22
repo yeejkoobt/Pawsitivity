1b09186e50152077159e0593bfbcc1da7f64a0c0
evillatoro1 <villatoroedwin1@gmail.com>
2016-10-22 10:44:08 -0400

    made most of the screens and some animation

--- app/src/main/java/com/catastrophic/pawsitivity/CustomizeCatActivity.java
+++ app/src/main/java/com/catastrophic/pawsitivity/CustomizeCatActivity.java
@@ -1,18 +1,13 @@
 package com.catastrophic.pawsitivity;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class CustomizeCatActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
-
-    public void handleLoginClicked(View view) {
-
+        setContentView(R.layout.activity_customize_cat);
     }
 }
--- app/src/main/java/com/catastrophic/pawsitivity/RegisterActivity.java
+++ app/src/main/java/com/catastrophic/pawsitivity/RegisterActivity.java
@@ -4,15 +4,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
     }
 
-    public void handleLoginClicked(View view) {
+    public void handleRegisterClicked(View view) {
 
     }
 }
