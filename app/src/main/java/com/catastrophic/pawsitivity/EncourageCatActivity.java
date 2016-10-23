e44f3a0e07188d610a36b69f91230a74907913d0
LILYLI <LILYLI>
2016-10-23 06:19:58 -0400

    Started encouragement cat activity

--- app/src/main/java/com/catastrophic/pawsitivity/EncourageCatActivity.java
+++ app/src/main/java/com/catastrophic/pawsitivity/EncourageCatActivity.java
@@ -9,5 +9,7 @@ public class EncourageCatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_encourage_cat);
+
+        String[]encouragementList = new String[58];
     }
 }
